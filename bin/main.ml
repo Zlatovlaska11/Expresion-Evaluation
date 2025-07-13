c93fcbe78615d0b90c853d7da12870ea37d6e007
Marian Pecqueur <marianpecqueur@Marians-MacBook-Air.local>
2025-07-13 16:32:24 +0200

    feat: idk

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,13 @@
 open Parser
 
 let () = 
-  Tokens.parse_input "1 + 2 - 3"
+  (* Tokens.parse_input "1 + 2 - 3" *)
+
+  let tokens = [Tokens.Token 'a'; Tokens.Operator '*'; Tokens.Token 'b'; Tokens.Operator '+'; Tokens.Token 'c'; Tokens.EOF] in 
+
+  let ast, _ = Tokens.parse_expresion tokens 0.0 in 
+
+  Tokens.print_expr ast
+
+
 
--- lib/tokens.ml
+++ lib/tokens.ml
@@ -3,6 +3,11 @@ type token =
   | Operator of char
   | EOF
 
+type expr = 
+  | Op of char
+  | Xpr of (char * expr) list
+
+
 let is_whitespace c =
   c == ' '
 
@@ -31,4 +36,51 @@ let parse_input data =
     |> List.filter (fun c -> not (is_whitespace c))
     |> List.map getTokenType in 
   
-  tokens |> List.iter print_token
+  tokens
+
+let getBindingPow token = 
+  match token with
+  | '-' | '+' -> 1.0
+  | '*' | '/' -> 2.0
+  | _ -> 0.0
+
+
+let rec parse_expresion tokens min_bp = 
+
+  let lhs, tokens = 
+    match tokens with
+    | Token c :: rest -> (Op c, rest)
+    | _ -> failwith "error unexpected token" 
+  in 
+
+  let rec loop lhs tokens = 
+
+    match tokens with
+    | Operator op :: rest -> 
+        let bp = getBindingPow op in
+
+        if bp < min_bp then 
+          (lhs, tokens)
+
+        else
+          
+          let rhs, tokens' = parse_expresion rest bp in 
+          let new_lhs = Xpr [ (op, lhs); (op, rhs) ] in 
+
+          loop new_lhs tokens'
+    | _ -> (lhs, tokens)
+  in
+  loop lhs tokens
+
+let rec print_expr ?(indent=0) e =
+  let pad = String.make indent ' ' in
+  match e with
+  | Op c ->
+      Printf.printf "%sOp(%c)\n" pad c
+  | Xpr lst ->
+      Printf.printf "%sXpr\n" pad;
+      List.iter (fun (op, sub) ->
+        Printf.printf "%s  Operator: %c\n" pad op;
+        print_expr ~indent:(indent + 4) sub
+      ) lst
+
